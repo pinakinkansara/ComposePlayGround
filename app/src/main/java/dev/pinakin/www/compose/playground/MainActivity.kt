12fdf313137b19f979ca82a99568e35e114052d0
Pinakin Kansara <pinakin.k.kansara@gmail.com>
2021-03-01 19:17:13 +0530

    Create README.md

--- app/src/main/java/dev/pinakin/www/compose/playground/MainActivity.kt
+++ app/src/main/java/dev/pinakin/www/compose/playground/MainActivity.kt
@@ -3,11 +3,19 @@ package dev.pinakin.www.compose.playground
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.pinakin.www.compose.playground.ui.theme.ComposePlayGroundTheme
 
 class MainActivity : AppCompatActivity() {
@@ -17,7 +25,7 @@ class MainActivity : AppCompatActivity() {
             ComposePlayGroundTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    NewStory()
                 }
             }
         }
@@ -25,14 +33,34 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun NewStory() {
+    MaterialTheme {
+        val typography = MaterialTheme.typography
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = "Story Image",
+                modifier = Modifier
+                    .height(180.dp)
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(4.dp)),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.requiredHeight(16.dp))
+            Text(text = "Pinakin Kansara", style = typography.h6)
+            Text(text = "B122, Prince Villa Phase 1", style = typography.body2)
+            Text(text = "Mobile: 999721222", style = typography.body2)
+        }
+    }
+
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun DefaultPreview() {
+fun NewStoryPreview() {
     ComposePlayGroundTheme {
-        Greeting("Android")
+        NewStory()
     }
 }
